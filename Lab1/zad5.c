eb5bc751d875e0d0219db43c53f90dfe13998677
Misha <mishakleban32@gmail.com>
2025-01-07 21:50:25 +0100

    add file

--- Lab1/zad5.c
+++ Lab1/zad5.c
@@ -1,28 +1,26 @@
-#include <stdio.h>
-
-int main() {
-    int num1, num2, num3;
-
-    // Запит трьох чисел у користувача
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-
-    printf("Enter the third number: ");
-    scanf("%d", &num3);
-
-    // Знаходимо найбільше число
-    if (num1 >= num2 && num1 >= num3) {
-        printf("The largest number is: %d\n", num1);
-    }
-    else if (num2 >= num1 && num2 >= num3) {
-        printf("The largest number is: %d\n", num2);
-    }
-    else {
-        printf("The largest number is: %d\n", num3);
-    }
-
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//    int num1, num2, num3;
+//
+//    printf("Enter the first number: ");
+//    scanf("%d", &num1);
+//
+//    printf("Enter the second number: ");
+//    scanf("%d", &num2);
+//
+//    printf("Enter the third number: ");
+//    scanf("%d", &num3);
+//
+//    if (num1 >= num2 && num1 >= num3) {
+//        printf("The largest number is: %d\n", num1);
+//    }
+//    else if (num2 >= num1 && num2 >= num3) {
+//        printf("The largest number is: %d\n", num2);
+//    }
+//    else {
+//        printf("The largest number is: %d\n", num3);
+//    }
+//
+//    return 0;
+//}
